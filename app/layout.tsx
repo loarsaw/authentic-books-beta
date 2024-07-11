b9d509c0ba7eb897fa1f895b0f1997f146416e07
loarsaw <amanahmed1385@gmail.com>
2024-07-11 16:28:26 +0530

    Donne

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
-'use client';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import '../styles/globals.css';
-import '@radix-ui/themes/styles.css';
 import { getGlobalData } from '../lib/cosmic';
 import Generator from 'next/font/local';
-import { Analytics } from '@vercel/analytics/react';
-import { SparklesCore } from '../components/Stars/Starts';
-import { Theme } from '@radix-ui/themes';
-import Timer from '../components/timer';
-import Sparkles from '../components/sparkles';
+import Header from '../components/Header';
 
 const sans = Generator({
   src: '../fonts/Generator-Variable.ttf',
@@ -16,9 +10,10 @@ const sans = Generator({
 });
 
 export async function generateMetadata() {
+  const siteData = await getGlobalData();
   return {
-    title: 'Authentic Books',
-    description: 'Feed your mind',
+    title: siteData.metadata.site_title,
+    description: siteData.metadata.site_tag,
   };
 }
 
@@ -27,38 +22,13 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-<<<<<<< HEAD
-=======
-  const socket: any = useRef();
-  const connectionUrl = 'http://3.110.216.79:5001';
-  // function connectSocket() {
-  //   socket.current = io(connectionUrl, {
-  //     transports: ['websocket'],
-  //     query: {
-  //       uid: '12092',
-  //     },
-  //   });
-  //   socket.current.on('connection');
-  // }
-  // useEffect(() => {
-  //   connectSocket();
-  // }, []);
+  const siteData = await getGlobalData();
 
->>>>>>> f064467 (new changes)
   return (
-    <html lang="en">
-      <body className=" ">
-        <Theme>
-          <div className=" relative bg-gray-900 dark:bg-zinc-950">
-            <div className="absolute h-full w-full">
-              <Sparkles />
-            </div>
-            <div className="absolute bottom-0 right-0">
-              <Timer />
-            </div>
-            <div className="">{children}</div>
-          </div>
-        </Theme>
+    <html lang="en" className={`${sans.variable} font-sans`}>
+      <body className="bg-white dark:bg-zinc-950">
+        <Header name={siteData} />
+        {children}
       </body>
     </html>
   );
