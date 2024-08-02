17b9a7819ea3e5b355ca6049e0762414fe7f2f9a
loarsaw <amanahmed1385@gmail.com>
2024-08-02 11:28:57 +0530

    removed ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import '../styles/globals.css';
 import { getGlobalData } from '../lib/cosmic';
 import Generator from 'next/font/local';
